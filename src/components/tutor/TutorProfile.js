a1ecf1fed4844e3ac17e6f26d9803a7a6b56b98b
Emily <emilyzhou.g@gmail.com>
2024-09-21 05:49:36 -0400

    initial commit

--- src/components/tutor/TutorProfile.js
+++ src/components/tutor/TutorProfile.js
@@ -1,33 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ProgressBar from "./progressBarComponent"; // Import the progress bar component
 import './progressBar.css';  
 
-const studentsData = [
-  { name: "John Doe", progress: 0.75 },
-  { name: "Jane Smith", progress: 0.45 },
-  { name: "Alice Johnson", progress: 0.90 },
-  { name: "Bob Williams", progress: 0.60 },
-];
+// const studentsData = [
+//   { name: "John Doe", progress: 0.75 },
+//   { name: "Jane Smith", progress: 0.45 },
+//   { name: "Alice Johnson", progress: 0.90 },
+//   { name: "Bob Williams", progress: 0.60 },
+// ];
 
-function tutorAdmin() {
+
+function TutorProfile() {
+  const [tutorsData, setTutors] = useState([])
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/tutors")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("got data")
+        var arr = Object.values(data)[0]
+        console.log(arr)
+        setTutors(arr);
+        console.log(tutorsData)
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div className="tutor-container">
-      <h1>Tutor: Student Progress</h1>
-
-      {/* Dynamically render student progress */}
-      {studentsData.map((student, index) => (
-        <div key={index} className="student-progress">
-          <span className="student-label">{student.name}</span> {/* Add student name */}
-          <ProgressBar
-            progressValue={student.progress}
-            backgroundClassName="progress-bar-background"
-            barClassName="progress-bar"
-            sidePadding="5px"
-          />
-        </div>
-      ))}
+      <h1>Tutors</h1>
+      <p>{tutorsData}</p>
+      
     </div>
   );
 }
 
-export default tutorAdmin;
+export default TutorProfile;
