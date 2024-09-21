b94d5c10f5562aebac3f315b01a34cd0ea53000b
Hugo Sandoval <sandovalhs@yahoo.com>
2024-09-21 03:17:50 -0400

    added dual os functionality

--- api/routers/dbinstance.py
+++ api/routers/dbinstance.py
@@ -1,9 +1,24 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import platform
+
+def get_os():
+    os_name = platform.system()
+    if os_name == "Windows":
+        return "Windows"
+    elif os_name == "Darwin":
+        return "macOS"
+    else:
+        return "Other OS"
 
 # Use a service account.
-cred = credentials.Certificate('..\\..\\firestore_credentials.json')
+OS = get_os()
+if OS == "Windows":
+    cred = credentials.Certificate('..\\..\\firestore_credentials.json')
+else:
+    cred = credentials.Certificate('../../firestore_credentials.json')
+
 
 app = firebase_admin.initialize_app(cred)
 
