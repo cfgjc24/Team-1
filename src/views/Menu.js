7cf9c07f699a3174ed7e0b4eda63fd66ea493e85
Emily <emilyzhou.g@gmail.com>
2024-09-20 21:35:09 -0400

    update menu

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Menu.css';
 
 const Menu = () => {
   return (
     <div className="menupage">
       {/* Main Section */}
-      <div className="intro">
-        <div className="intro-content">
-          <Link to="/student">
-            <button className="cta-button">Student</button>
+      <div className="introMenu">
+        <div className="intro-content-menu">
+          <Link to="/student" class="link">
+            <button className="nav-button">Student Dashboard</button>
           </Link>
-          <Link to="/tutor">
-            <button className="cta-button">Tutor</button>
+          <Link to="/tutor" class="link">
+            <button className="nav-button">Tutor Dashboard</button>
           </Link>
-          <Link to="/admin">
-            <button className="cta-button">Admin</button>
+          <Link to="/admin" class="link">
+            <button className="nav-button">Admin Dashboard</button>
           </Link>
         </div>
       </div>
